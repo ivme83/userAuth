e87b77f762c4cde7474e2309d2c1905cb3140471
Roland Willis <rolandwillisiv@gmail.com>
2018-05-19 16:11:38 -0700

    Building for production

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import Login from './components/Login';
 import Register from './components/Register';
 
@@ -19,4 +19,4 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-registerServiceWorker();
+// registerServiceWorker();
