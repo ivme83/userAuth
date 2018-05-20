5c33203934f1d44ba7d164088a40592282accef7
Roland Willis <rolandwillisiv@gmail.com>
2018-05-19 21:33:47 -0700

    Building for production

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const app             = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/book', book);
 app.use('/api/auth', auth);
